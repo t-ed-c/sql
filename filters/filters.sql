611ead11e4f4d20beaf9b1586496c781a268d84e
t-ed-c <ted.kimemia23@students.dkut.ac.ke>
2025-09-29 20:45:29 +0300

    FILTERING AND SEARCHING DATA

--- filters/filters.sql
+++ filters/filters.sql
@@ -1,5 +1,5 @@
 -- 1. Simple filter
-SELECT * FROM students WHERE age > 20;
+SELECT AGE FROM students WHERE age > 20;
 
 -- 2. Combining conditions
 SELECT * FROM students WHERE course = 'Database Systems' AND age < 21;
@@ -11,7 +11,9 @@ SELECT * FROM students WHERE age BETWEEN 20 AND 22;
 SELECT * FROM students WHERE course IN ('Database Systems', 'Networking');
 
 -- 5. LIKE
-SELECT * FROM students WHERE first_name LIKE 'A%';
+SELECT * FROM students WHERE first_name LIKE 'B%';
 
 -- 6. ORDER BY
 SELECT * FROM students ORDER BY age DESC;
+
+SELECT * FROM students WHERE NOT course = 'Networking';
